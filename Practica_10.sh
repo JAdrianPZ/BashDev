fd0d7a2731c4a6a65ea340463bf6f9f6227a08fa
Moises Tapia <moisestapia741@gmail.com>
2019-09-26 14:56:28 -0500

    Create Practica_10.sh

---
+++ Practica_10.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+USERNAME=$1
+PASSWORD=$2
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "      the following Uername is $USERNAME and password is $PASSWORD"
+echo "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
