ccf60b9f3ba171c3d6657cea5880af5b8d041025
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-30 16:27:05 -0500

    Create Ejercicio_04.sh

---
+++ Ejercicios/Ejercicio_04.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#this line is intended to be used as general description of this script
+#and anything that it does                                                                                                                           
+                                                                                                                                                     
+clear #clears the screen                                                                                                                             
+                                                                                                                                                     
+MYUSERNAME="terry" #THE  USERNAME FOR THIS APPLICATION                                                                                               
+                                                                                                                                                     
+echo "We are using the default uer called : $MYUSERNAME" #display to the conosole                                                                    
+                                                                                                                                                     
+DATETIMESTAMP=`date`                                                                                                                                 
+                                                                                                                                                     
+echo "this is when the script was run: $DATETIMESTAMP" #this is the timesamp of run                                                                  
+    
