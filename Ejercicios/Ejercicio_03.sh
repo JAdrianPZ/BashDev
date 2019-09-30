6f04e24d006e9d919d8721b0203a1c5441c3527c
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-30 16:25:51 -0500

    Create Ejercicio_03.sh

---
+++ Ejercicios/Ejercicio_03.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MYUSERNAME="username"
+MYPASSWORD="password123"
+STARTSCRIPT=`date`
+echo "M login name for this application is: $MYUSERNAME"
+echo "My login password for this application is:$MYPASSWORD"
+echo "I started this script at: $STARTSCRIPT"
+
+ENDOFSCRIPT=`date`
+
+echo "I ended this script at:$ENDOFSCRIPT"
