9be270367bcce8ce3c686f63ff4806389ad643d4
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-30 16:29:26 -0500

    Create Ejercicio_07.sh

---
+++ Ejercicios/Ejercicio_07.sh
@@ -0,0 +1,5 @@
+#!/bin/bash                                                                                                                                          
+                                                                                                                                                     
+expr 2 + 2                                                                                                                                           
+expr 2 + 2 \* 4                                                                                                                                      
+expr \( 2 + 2 \) \* 4  
