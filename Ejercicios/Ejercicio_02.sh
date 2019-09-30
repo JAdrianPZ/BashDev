d2e4a34d3f75c2770461cc1d1dffe35e0bea744a
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-09-30 16:24:16 -0500

    Create Ejercicio_02.sh

---
+++ Ejercicios/Ejercicio_02.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give us enviorment information"
+echo "==============================================="
+echo ""
+echo "Hello Username: $USER"
+echo ""
+echo "Your home directory is $HOME"
+echo ""
+echo "Your History file will ignore: $HISTCONTROL"
+echo ""
+echo "Your Teminal session type is: $TERM"
+echo ""
