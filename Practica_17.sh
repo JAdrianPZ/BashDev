73e25314e8f2707fec31a9e0c7b0b9061d1294d7
JAdrianPZ <45021900+JAdrianPZ@users.noreply.github.com>
2019-10-02 12:40:35 -0500

    Update Practica_17.sh

--- Practica_17.sh
+++ Practica_17.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-EXT=("q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "a" "s" "d" "f" "g" "h" "j" "k" "l" "z" "x" "c" "v" "b" "n" "m" "qq" "ww" "ee" "rr")
+ EXT=("q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "a" "s" "d" "f" "g" "h" "j" "k" "l" "z" "x" "c" "v" "b" "n" "m" "qq" "ww" "ee" "rr" "tt" "yy")
 
-#for COUNT in ${EXT[@]};
-#do
-#mkdir "$COUNT"
-#done
+ for COUNT in ${EXT[@]};
+ do
+ mkdir "$COUNT"
+ done
 
-
-for COUNT in ${EXT[@]};
-do
-mv "*.$COUNT $COUNT"
+for COUNT2 in ${EXT[@]};
+ do
+mv *.$COUNT2 $COUNT2
 done
-
